b163fe991631617495aac7ca55db4255e06fa547
Mehmet Kaplan <mehmet.kaplan@gmail.com>
2023-04-30 00:42:48 +0300

    export method test

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -14,7 +14,7 @@ import {
 
 import { StripeActionPage } from './stripe-action-page';
 
-export default {
+export {
 	init,
 	generateCustomer,
 	getCustomer,
@@ -25,6 +25,6 @@ export default {
 	getAccount,
 	oneTimePayment,
 	getOneTimePaymentStatus,
-	StripeActionPage: StripeActionPage,
+	StripeActionPage,
 	exportedForTesting,
 };
